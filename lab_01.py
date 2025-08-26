33df6f44f883b55a6ac4d64ecd5d341dab04e2bd
jmmy1e <103778480+jmmy1e@users.noreply.github.com>
2025-08-25 22:46:55 -0700

    Update lab_01.py
    -made ranges inclusive (randrange to randint), before it was only going to 99 as max
    -removed redundant casts after changing randrange
    -corrected attempts accounted 1 to 0
    -grammar and syntax changes
    -hide debug output for submission

--- lab_01.py
+++ lab_01.py
@@ -1,9 +1,10 @@
-"""
-Name: Jimmy Le, Daniel McCray
-Date: Aug 25 2025
-Description: Our first Lab demonstrating the usage of using input methods and logic
-to create a simple guessing game
+""" LAB #1
+    08/25/2025
 
+    Student 1: Daniel, Mccray
+    Student 2: Jimmy, Le
+    Description: Our first Lab demonstrating the usage of using input methods and logic
+    to create a simple guessing game from 1-100
 """
 
 import random
@@ -25,28 +26,31 @@ def main():
    # ? Gen the random number from ranges 1-100 inclusively. 
    # ? ONLY ONCE at the beggining of the function
    """
-   random_number = int(random.randrange(1,100))
+   random_number = random.randint(1, 100)
    
-   # ! Uncomment the following to test
-   print(random_number)
+   # Uncomment for debugging
+   # print(random_number)
    
    # ? Create a counter to keep track of the attempts
-   counter = 1
+   counter = 0
    
    print("--- Guessing Game Has Started ---")
   
-   # * Uses a while loop until the user’s guess is
+   # * Uses a while loop until the user’s guesses correct
    while True:
       
       # ? Use check_input module to ensure proper range enforcement until correct input
-      current_input = int(get_int_range("Please enter a number: ",low_number,high_number))
+      current_input = get_int_range("Please enter a number (1-100): ", low_number, high_number)
+            
+      # ? count the valid guesses immediately to include correct guess
+      counter += 1
       
       # ? Uses if statements to check high or too low.
       if current_input == random_number:
-         print(f"Congratulations, you did it in {counter} tries")
+         print(f"Congratulations, you did it in {counter} attempts.")
          break
       elif current_input < random_number:
-          print(f"Too low! Guess again ({low_number}-{high_number}) ")
+          print(f"Too low! Guess again ({low_number}-{high_number})")
       elif current_input > random_number:
          print(f"Too high! Guess again ({low_number}-{high_number})")
      
@@ -55,7 +59,6 @@ def main():
       Because get_int_range enforces input correction, 
       we can safely increment the counter without having to worry
       """
-      counter +=1
     
     
     
